58ca535b23dd0c10b66635de955df2107277a607
Mike Buerli <mbuerli@gmail.com>
2013-01-23 02:26:21 -0800

    profile/main updates

--- routes/profile.rb
+++ routes/profile.rb
@@ -18,12 +18,8 @@ class Assassins < Sinatra::Application
                 # database troubles
             end
 
-            @friends = @graph.get_connections('me', 'friends')
-            @photos  = @graph.get_connections('me', 'photos')
-            @likes   = @graph.get_connections('me', 'likes').first(4)
-
             # for other data you can always run fql
-            @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
+            @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1").first(12)
             erb :profile
         else
             redirect "/"
